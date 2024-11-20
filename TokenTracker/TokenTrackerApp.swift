9b5e85a9e72f1c2fae9b1d7c8120891492cf7a71
Chudotvorets <simonovevgvlad@gmail.com>
2024-11-20 11:53:05 +0400

    Initial Commit

---
+++ TokenTracker/TokenTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenTrackerApp.swift
+//  TokenTracker
+//
+//  Created by dev on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TokenTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TokenTrackerUITests/TokenTrackerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TokenTrackerUITestsLaunchTests.swift
+//  TokenTrackerUITests
+//
+//  Created by dev on 20.11.2024.
+//
+
+import XCTest
+
+final class TokenTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
