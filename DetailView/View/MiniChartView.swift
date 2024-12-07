2c2b702fbb8c8acf8c65668035e6e2ebf15b07a3
Chudotvorets <simonovevgvlad@gmail.com>
2024-12-07 19:18:45 +0400

    добавил miniChartView

---
+++ DetailView/View/MiniChartView.swift
@@ -0,0 +1,8 @@
+//
+//  MiniChartView.swift
+//  TokenTracker
+//
+//  Created by dev on 07.12.2024.
+//
+
+import Foundation
---
+++ Extensions/Double.swift
@@ -0,0 +1,8 @@
+//
+//  Double.swift
+//  TokenTracker
+//
+//  Created by dev on 04.12.2024.
+//
+
+import Foundation
